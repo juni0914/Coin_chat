de7d3c08f0edbad7502aba0bf68c81b44deda14f
juni0914 <juni0914@naver.com>
2022-12-07 20:56:09 +0900

    2022-12-07/20:56

--- src/components/ChatPage/ChatPage.js
+++ src/components/ChatPage/ChatPage.js
@@ -20,7 +20,7 @@ function ChatPage() {
                     key={currentChatRoom && currentChatRoom.id}
                 />
               </div>
-              <div style={{ width: '150vw' }}>
+              <div style={{ width: '250vw' }}>
                 <Secondpage
                 />
               </div>
